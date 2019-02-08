8a0f335b9007f9ceb04abf96011845ea8b292a86
iCurvaceous <alexandra.da.decastro@gmail.com>
2019-02-08 03:01:36 -0500

    Working Handlebar and Bootstrap Pages

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
+//API's
 var express = require('express');
+var app = express();
 var router = express.Router();
-var mongo = require('mongodb');
+var exphbs = require('express-handlebars');
 
+var bodyParser = require('body-parser');
+
+var url = 'mongodb://localhost:27017/dts';
+var mongoose = require('mongoose');
 //assert is a built in packaging nodejs normally used for testing
 //allows us to compare values etc. 
 //use this when connect to mongoDB database or operations
 //to check if everything went right
 var assert = require('assert');
-var path = 5000;
+var port = 5000;
 
+//security login & password encryption(hash)
+//var passport = require('passport');
+//var {ensureAuthenticated} = require('./helpers/auth');
 
+//gets rid of warning for Mongoose
+mongoose.Promise = global.Promise
+
+//connect to mongodb using mongoose
+mongoose.connect(url,{
+    useMonogoClienct:true
+})
+.then(function(){console.log("MongoDB Connected.")})
+.catch(function(err){console.log(err)});
+
+//sets up handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Functions needed to run body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Route to index
 router.get('/', function(req, res, next){
+    res.render('index');
+});
+
+//Route to login
+router.get('/login', function(req,res){
+    res.render('login');
+});
+
+//Route to login
+router.get('/register', function(req,res){
+    res.render('register');
+});
+
+router.post('/get-data', function(req, res, next){
 
 });
 
@@ -28,3 +72,13 @@ router.post('/delete', function(req, res, next){
 
 //makes router public
 module.exports = router;
+module.exports = mongoose;
+
+//routes for paths
+app.use(express.static(__dirname+'/views'));
+app.use('/', router);
+
+//starts server
+app.listen(port, function(){
+    console.log("Server is running on Port " + port);
+});
---
+++ models/UsersSchema.js
@@ -0,0 +1,34 @@
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    job:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+mongoose.model('Users', UserSchema);
